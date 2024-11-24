1f855e6f070533aa762231308ae30e42a431fce9
ArmanM26 <armanmkrtchyan260612@gmail.com>
2024-11-25 00:10:30 +0400

    Creating initial files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import CryptoList from './pages/cryptoList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CryptoList />
+    </div>
+  );
+}
+
+export default App;
